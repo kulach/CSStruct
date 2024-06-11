7a5d93335ac5cbef2043f5211880942210db08f4
Kulach <patrickkulach54@gmail.com>
2024-06-11 09:09:53 -0500

    Test directory

---
+++ test/test_structs.h
@@ -0,0 +1,16 @@
+#include <utest/utest.h>
+#include <csstruct.h>
+#include <stdint.h>
+
+struct IntegerStruct {
+    uint8_t u8 = 0;
+    int8_t i8 = 0;
+    uint16_t u16 = 0;
+    int16_t i16 = 0;
+    uint32_t u32 = 0;
+    int32_t i32 = 0;
+    uint64_t u64 = 0;
+    uint64_t i64 = 0;
+
+
+}
---
+++ test/tests.cpp
@@ -0,0 +1,7 @@
+#include <utest/utest.h>
+
+UTEST(UTestsTests, BaseTest) {
+    EXPECT_TRUE(true);
+}
+
+UTEST_MAIN();
